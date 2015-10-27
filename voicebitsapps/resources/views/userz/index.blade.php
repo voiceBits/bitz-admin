9188728f048ad707de2de029928291289c828d41
Kevin L Hall <kevin@voicebitz.com>
2015-10-27 22:20:16 +0100

    Creating Userz (voicebitz) listing

---
+++ voicebitsapps/resources/views/userz/index.blade.php
@@ -0,0 +1,15 @@
+@extends('common.layout')
+@section('title')
+Inspire. Be inspired.
+@stop
+@section('body')
+<div class="row">
+    <ul class="collection">
+@foreach ($users as $user)
+        <li class="collection-item dismissable">
+            <div>{{ $user->username }}<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div>
+        </li>
+@endforeach 
+    </ul>
+</div>
+@stop
