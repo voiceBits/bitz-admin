a39f9e190e59d88966ad9027a2e9dcd9c5d86a0e
Kevin L Hall <kevin@voicebitz.com>
2015-10-27 22:31:24 +0100

    Cleaning up routes

--- voicebitsapps/app/Http/routes.php
+++ voicebitsapps/app/Http/routes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -10,104 +9,28 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+# Admin Login
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+]);
 
+# Main
 Route::get('/', 'WelcomeController@index');
+Route::get('home', 'HomeController@index'); // authenticated users home
 
-Route::get('home', 'HomeController@index');
-Route::get('userz', 'HomeController@userz');
+# Blog pages
+Route::get('about', 'WelcomeController@about');
+Route::get('blog', 'WelcomeController@blog');
 
 // Model bind and Resource routes
 
     # Users
     Route::model('users', 'App\User');
     Route::resource('users', 'UsersController');
-
-Route::get('profile', 'HomeController@memberprofile');
-Route::get('members', 'PageController@home');
-Route::get('tasks', 'TaskController@index');
-
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
-
-//Route::get('/', 'PageController@index');
-Route::get('about', 'PageController@about');
-Route::get('login', 'PageController@login');
-//Route::get('home', 'PageController@home');
-// Intervention Image 2.x Library
-/* Try the same thing with videos????? 
-Route::get('pinduin_orig', function()
-{
-    $img = Image::make('images/peggy_penguin_2.png')->resize(60, 60);
-
-    return $img->response('png');
-});
-*/
-// consider using storage as the file location like this
-//
-Route::get('swipeggy', function()
-{
-    $img = Image::make('images/peggy_penguin_2.png')->resize(90, 90);
-
-    return $img->response('png');
-});
-Route::get('photos', function()
-{
-    $img = Image::make('images/peggy_penguin_2.png')->resize(80, 80);
-
-    return $img->response('png');
-});
-Route::get('peggyicon-medium', function()
-{
-    $img = Image::make('images/peggy_penguin_2.png')->resize(55, 55);
-
-    return $img->response('png');
-});
-Route::get('photos/{filename}', function($filename)
-{
-	if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
-    $path = 'images' . DS . $filename;
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $img = Image::make($file);
-
-    return $img->response($type);
-});
-
-Route::get('photos/thumbnails/{filename}', function($filename)
-{
-	if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
-    $path = 'images' . DS . $filename;
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $img = Image::make($file)->resize(60, 60);
-
-    return $img->response($type);
-});
-
-Route::get('thumbnails/{filename}', function($filename)
-{
-	if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
-    $path = 'images' . DS . $filename;
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $img = Image::make($file)->resize(120, 120);
-
-    return $img->response($type);
-});
-Route::get('posters/{filename}', function($filename)
-{
-	if (!defined('DS')) define('DS', DIRECTORY_SEPARATOR);
-    $path = 'images' . DS . $filename;
-    $file = File::get($path);
-    $type = File::mimeType($path);
-
-    $img = Image::make($file)->resize(240, 240);
-
-    return $img->response($type);
-});
+    
+    # voicebitZ tables - Authenticated users only
+    Route::get('userz', 'HomeController@userz');
+    Route::get('bitz', 'HomeController@voizbitz');
+    Route::get('cloudz', 'HomeController@cloudz');
+    Route::get('peepz', 'HomeController@peepz');
