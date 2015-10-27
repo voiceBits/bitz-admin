fcec86007f6e5cfd00de86156b6878f94433a743
Kevin L Hall <kevin@voicebitz.com>
2015-10-27 22:08:49 +0100

    Creating dashboard controller

--- voicebitsapps/app/Http/Controllers/HomeController.php
+++ voicebitsapps/app/Http/Controllers/HomeController.php
@@ -33,23 +33,19 @@ class HomeController extends Controller {
 	 */
 	public function index()
 	{
-		$userscount = DB::select('select count(*) as count from users where status = ?', ['Y']);
-		$pinduinscount = $userscount[0]->count;
-
-		return view('home',compact('pinduinscount'));
+		return view('home');
 	}
-	
-    public function memberprofile()
-    {
-        // Just testing
-		$membersInfo = ['Coach', 'Infamusk72', 'The General'];
-		//$members = DB::select('select * from members where type = ?', ['earlybird']);
-		//$users = DB::select('select * from users where status = ?', ['Y']);
-		//$membersInfo = DB::select('select * from users, members where users.id = members.id_users and users.status = ?', ['Y']);
-//		var_dump($membersInfo);
-//		var_dump(compact('members', 'users'));
-		//var_dump (compact('members'));
-		//exit;
-		return view('members.home',compact('membersInfo'));
-    }
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function userz()
+	{
+		//
+            $users = DB::select('select * from userz');
+            return view('userz.index',compact('users'));
+	}
+
 }
