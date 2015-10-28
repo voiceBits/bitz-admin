e1b11456806e7915e223cd39a61452dc6a1a1a35
Kevin L Hall <kevin@voicebitz.com>
2015-10-28 14:30:57 +0100

    Adding Show and Delete links

--- voicebitsapps/resources/views/users/index.blade.php
+++ voicebitsapps/resources/views/users/index.blade.php
@@ -12,7 +12,12 @@ Inspire. Be inspired.
     <ul class="collection">
 @foreach ($users as $user)
         <li class="collection-item dismissable">
-            <div>{{ $user->username }}<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div>
+            <div>{{ $user->username }}
+                <a href="{{ action('UsersController@show', [$user->id]) }}" class="secondary-content"><i class="material-icons">send</i></a>
+                {!! Form::open(['action' => ['UsersController@destroy', $user->id], 'method' => 'delete']) !!}
+                  {!! Form::submit('delete', ['class'=>'red material-icons']) !!}
+                {!! Form::close() !!}                
+            </div>
         </li>
 @endforeach 
     </ul>
